cace4fbbbc3cd3985ece20888c4736f9d41ad82b
muzaffar ahmed <nobin_probin@hotmail.com>
2020-06-09 12:21:36 -0400

    Revert "modified romanToInteger"
    This reverts commit 26ceac28f4083ffa0a7d6e045119a0b05dc21472.

--- RomanToInteger/src/Roman_To_Int.java
+++ RomanToInteger/src/Roman_To_Int.java
@@ -1,35 +1,49 @@
 public class Roman_To_Int {
      public static void main(String[]args){
-         System.out.println(romanToInt("MDCXCV"));
+
      }
 
-     public static int romanToInt(String s) {
-         int result = 0, first_ch, next_ch =0;
-         for (int i = 0; i < s.length(); i++)
-         {
-             first_ch = value(s.charAt(i));
-             if (i+1 < s.length()) {
-                 next_ch = value(s.charAt(i + 1));
+     public int romanToInt(String s) {
+       int number = 0;
 
-                 if (first_ch < next_ch) {
-                     result += next_ch - first_ch;
-                     i++;
-                 } else result += first_ch;
-             }
-                    else result += first_ch;
-         }
-         return result;
+
+
+       for (int i = 0; i < s.length(); i++){
+           char first_ch = ch(s.charAt(i));
+           char next_ch = ch(s.charAt(i+1));
+       }
+         char ch;
+
+
      }
 
-    private static int value(char symbol) {
-          if (symbol == 'I') return 1;
-          else if (symbol== 'V') return 5;
-          else if (symbol == 'X') return 10;
-          else if (symbol == 'L') return 50;
-          else if (symbol == 'C') return 100;
-          else if (symbol == 'D') return 500;
-          else if (symbol == 'M') return 1000;
-          else return 0;
-     }
+    public char ch(char symbol) {
+        int number = 0;
+
+        switch (symbol){
+            case "V":
+                number = 5;
+                break;
+            case "X":
+                number = 10;
+                break;
+            case "L":
+                number = 50;
+                break;
+            case "C":
+                number = 100;
+                break;
+            case "D":
+                number = 500;
+                break;
+            case "M":
+                number = 1000;
+                break;
+            default:
+                number = 0;
+
+        }
+        return symbol;
+    }
 
 }
